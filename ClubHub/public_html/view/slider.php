a2a2a8d2db1f5da256b2f52ee6bb74ea7d80d385
marcooliveira04 <marco_oliveira94@live.com>
2017-10-23 20:23:50 -0200

    Alterações Slider
    Troca de imagens do slider para visualização da @SandraPavan

--- ClubHub/public_html/view/slider.php
+++ ClubHub/public_html/view/slider.php
@@ -6,25 +6,25 @@
 	</ol>
 	<div class="carousel-inner">
 		<div class="carousel-item active">
-			<img class="d-block w-100" src="https://images.blog.whsmith.co.uk/leadimage/hp-cc-signed-comp.jpg" alt="First slide">
-			<div class="carousel-caption d-none d-md-block text-right">
+			<img class="d-block w-100" src="./view/layout/images/slideshowvinho1.png" alt="First slide">
+<!-- 			<div class="carousel-caption d-none d-md-block">
 				<h3>...</h3>
 				<p>...</p>
-			</div>
+			</div> -->
 		</div>
 		<div class="carousel-item">
-			<img class="d-block w-100" src="https://compass-ssl.xbox.com/assets/6c/d9/6cd9e5f2-8f8e-47f3-b3a2-7edcd2ebe21f.jpg?n=ME-SOW_Page-Hero-1084_1920x600.jpg" alt="Second slide">
-			<div class="carousel-caption d-none d-md-block text-right">
-				<h3>Receba esse mês o game Shadow of War</h3>
+			<img class="d-block w-100" src="./view/layout/images/slideshowvinho2.png" alt="Second slide">
+<!-- 			<div class="carousel-caption d-none d-md-block">
+				<h3 class="text-white">Receba esse mês o game Shadow of War</h3>
 				<p>Caminhe com Celebrimbor</p>
-			</div>
+			</div> -->
 		</div>
 		<div class="carousel-item">
 			<img class="d-block w-100" src="https://compass-ssl.xbox.com/assets/23/24/2324ec96-3670-4d9f-b06f-756d8247687a.jpg?n=ACH_GLP-Page-Hero-1084_1920x600_02.jpg" alt="Third slide">
-			<div class="carousel-caption d-none d-md-block">
+<!-- 			<div class="carousel-caption d-none d-md-block">
 				<h3>...</h3>
 				<p>...</p>
-			</div>
+			</div> -->
 		</div>
 	</div>
 	<a class="carousel-control-prev" href="#indicadores" role="button" data-slide="prev">
@@ -36,3 +36,9 @@
 		<span class="sr-only">Próximo</span>
 	</a>
 </div>
+
+<style type="text/css">
+	.bg-gray{
+		background-color: rgba(52,58,64, .1);
+	}
+</style>
